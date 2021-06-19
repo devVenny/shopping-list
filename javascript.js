322528ecbf8673755e8176b7e6ebe626f2528599
vennylee <hyungsup42134@gmail.com>
2021-06-19 20:03:12 +0900

    [JS]Add shopping lists
    Load input's value to make a list with creating new elements('li', 'span',  'button').

---
+++ javascript.js
@@ -0,0 +1,36 @@
+// 1. Input의 value 받아서 화면에 list로 출력.
+// 2. Delete 버튼 눌러서 아이템 한 개 삭제.
+// 3. plus-circle 아이콘 클릭하여 모든 아이템 삭제.
+// 4. 목록이 화면에 가득찼을 떄 스크롤 기능 추가.
+
+'user strict'
+
+const form = document.querySelector('.footer');
+const input = document.querySelector('.input');
+const items = document.querySelector('.list__items');
+
+function paintList(text){
+    const li = document.createElement("li");
+    li.setAttribute('class', 'item');
+
+    const span = document.createElement("span");
+    span.setAttribute('class', 'item__name');
+    span.textContent=`- ${text}`;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.setAttribute('class', 'item__button');
+    deleteBtn.textContent='X'
+
+    li.append(span);
+    li.append(deleteBtn);
+    items.append(li);
+}
+
+function onAdd(e){
+    e.preventDefault();
+    const currentValue = input.value;
+    paintList(currentValue);
+    input.value='';
+}
+
+form.addEventListener('submit', onAdd);
